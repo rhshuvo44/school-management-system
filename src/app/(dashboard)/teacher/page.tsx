a5c78c187d7b4d8a7809cab6667114e727459859
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2025-01-07 17:59:18 +0600

    fact:Parent dashboard schedule

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,19 +1,18 @@
 import Announcements from "@/components/modules/dashboard/Announcements";
-import EventCalender from "@/components/modules/dashboard/EventCalender";
 import BigCalender from "@/components/modules/student/BigCalender";
 
 const TeacherPage = () => {
   return (
-    <div className="p-4 flex gap-4 flex-col xl:flex-row">
-      {/* Left side bar */}
-      <div className="w-full xl:w-2/3 flex flex-col gap-4">
-        <div className="h-full bg-white rounded-md p-4">
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
           <h1 className="text-xl font-semibold">Schedule</h1>
           <BigCalender />
         </div>
       </div>
-      {/* Right side bar */}
-      <div className="w-full xl:w-1/3">
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8 min-h-screen">
         <Announcements />
       </div>
     </div>
--- src/components/modules/student/BigCalender.tsx
+++ src/components/modules/student/BigCalender.tsx
@@ -18,7 +18,7 @@ const BigCalender = () => {
       endAccessor="end"
       views={[Views.WORK_WEEK, Views.DAY]} // Use Views constants
       view={view} // Bind to state
-      style={{ height: "98%" }}
+      style={{ height: "95%" }}
       onView={onViewChange}
       min={new Date(2025, 0, 1, 8, 0, 0)} // Start time at 8:00 AM
       max={new Date(2025, 0, 1, 17, 0, 0)} // End time at 5:00 PM

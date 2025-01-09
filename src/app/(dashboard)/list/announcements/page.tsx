7843b0d48db17daa76b741fdbe3f0bed810085f9
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2025-01-09 18:17:57 +0600

    fix:announcement

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -5,12 +5,10 @@ import TableSearch from "@/components/ui/TableSearch";
 import { role } from "@/lib/data";
 import prisma from "@/lib/prisma";
 import { ITEM_PER_PAGE } from "@/lib/settings";
-import { announcementColumns } from "@/types/tableColum";
 import { Announcement, Class, Prisma } from "@prisma/client";
 import Image from "next/image";
-type AnnouncementList = Announcement & {
-  class: Class;
-};
+
+type AnnouncementList = Announcement & { class: Class };
 const AnnouncementListPage = async ({
   searchParams,
 }: {
@@ -21,10 +19,34 @@ const AnnouncementListPage = async ({
   // const currentUserId = userId;
   const currentUserId = "student1";
 
+  const columns = [
+    {
+      header: "Title",
+      accessor: "title",
+    },
+    {
+      header: "Class",
+      accessor: "class",
+    },
+    {
+      header: "Date",
+      accessor: "date",
+      className: "hidden md:table-cell",
+    },
+    ...(role === "admin"
+      ? [
+          {
+            header: "Actions",
+            accessor: "action",
+          },
+        ]
+      : []),
+  ];
+
   const renderRow = (item: AnnouncementList) => (
     <tr
       key={item.id}
-      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purpleLight"
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
     >
       <td className="flex items-center gap-4 p-4">{item.title}</td>
       <td>{item.class?.name || "-"}</td>
@@ -95,42 +117,31 @@ const AnnouncementListPage = async ({
   ]);
 
   return (
-    <div className="flex-1 p-4 gap-4 bg-white rounded-md m-4 mt-0">
-      {/* TOP  */}
-      <div className="flex justify-between items-center">
+    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+      {/* TOP */}
+      <div className="flex items-center justify-between">
         <h1 className="hidden md:block text-lg font-semibold">
           All Announcements
         </h1>
-        <div className="flex flex-col md:flex-row gap-4 items-center  w-full md:w-auto">
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
-            <button className="w-8 h-8 bg-yellow rounded-full flex items-center justify-center">
-              <Image src="/filter.png" width={14} height={14} alt="add" />
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/filter.png" alt="" width={14} height={14} />
             </button>
-            <button className="w-8 h-8 bg-yellow rounded-full flex items-center justify-center">
-              <Image src="/sort.png" width={14} height={14} alt="add" />
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
-            {role === "admin" && <FormModal table="assignment" type="create" />}
+            {role === "admin" && (
+              <FormModal table="announcement" type="create" />
+              // <FormContainer table="announcement" type="create" />
+            )}
           </div>
         </div>
       </div>
-      {/* LIST  */}
-      <Table
-        columns={[
-          ...announcementColumns,
-          ...(role === "admin"
-            ? [
-                {
-                  header: "Actions",
-                  accessor: "action",
-                },
-              ]
-            : []),
-        ]}
-        renderRow={renderRow}
-        data={data}
-      />
-      {/* PAGINATION  */}
+      {/* LIST */}
+      <Table columns={columns} renderRow={renderRow} data={data} />
+      {/* PAGINATION */}
       <Pagination page={p} count={count} />
     </div>
   );
